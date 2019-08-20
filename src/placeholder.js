e4c78678b68a9a75dae82616481d3d604d1929a1
jimihunter2002 <jimi.hunter008@gmail.com>
2019-08-20 10:58:34 +0100

    removed comment

--- src/placeholder.js
+++ src/placeholder.js
@@ -2,37 +2,6 @@ const axios = require('axios');
 const baseUrl = 'https://jsonplaceholder.typicode.com'
 const placholder = {
 
-    /*createUser: (input) => axios.post(`${baseUrl}/users`, input)
-        .then(response => response)
-        .catch(err => err),
-
-    getUser: (userId) => axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
-        .then(response => response)
-        .catch(err => err),
-
-    createPost: (payload) => axios.post('https://jsonplaceholder.typicode.com/posts', payload)
-        .then(response => response)
-        .catch(err => err),
-
-    getPost: (postId) => axios.get(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-        .then(response => response)
-        .catch(err => err),
-
-    updatePost: (payload, postId, options) => axios.put(`https://jsonplaceholder.typicode.com/posts/${postId}`, payload, options)
-        .then(response => response)
-        .catch(err => err),
-
-    partialPostUpdate: (payload, postId) => axios.patch(`https://jsonplaceholder.typicode.com/posts/${postId}`, payload)
-        .then(response => response)
-        .catch(err => err),
-
-    deletePost: (postId) => axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-        .then(response => response)
-        .catch(err => err), */
-
-
-
-
     createUser: (payload) => axios.post(`${baseUrl}/users`, payload)
         .then(response => response)
         .catch(err => err),
